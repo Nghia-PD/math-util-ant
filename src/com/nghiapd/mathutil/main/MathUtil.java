96b712bcbaf72467fcf031d6a65d735d24aa5634
Nghia-PD <nghia01694049627@gmail.com>
2022-10-14 10:38:54 +0700

    update getf() to recursive algorithm

--- src/com/nghiapd/mathutil/main/MathUtil.java
+++ src/com/nghiapd/mathutil/main/MathUtil.java
@@ -24,9 +24,9 @@ public class MathUtil {
     
     public static long getFactorial(int n) {
         //if n <0 va n > 20
-        if (n < 0 || n > 20) {
-            throw new IllegalArgumentException("Invalid n. N must be between 0 -> 20");
-        }
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid n. N must be between 0 -> 20");
+//        }
         if (n == 0 || n == 1) {
             return 1; //chốt dừng đệ quy
         }
